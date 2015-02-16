caf438dc7bac41217572452c829a1152b03c5003
Morozov Andrew <anmorozov23@yandex.ru>
2015-02-16 12:34:19 +0400

    use jquery codestyle; remove caret factory; jsdoc tiny fix

--- caret.jquery.js
+++ caret.jquery.js
@@ -1,6 +1,6 @@
-(function ($) {
-    $.fn.caret = function (position) {
-        var caret = getCaret(this[0]);
+(function($) {
+    $.fn.caret = function(position) {
+        var caret = new Caret(this[0]);
         if (position) {
             caret.setPosition(position);
             return this;
@@ -10,33 +10,14 @@
     };
 
     /**
-     * Caret factory.
-     */
-    function getCaret(element) {
-        this._index = this._index || [];
-        this._store = this._store || [];
-
-        var index = this._index.indexOf(element);
-        if (index === -1) {
-            index = this._index.push(element) - 1;
-            this._store[index] = new Caret(element);
-        }
-
-        return this._store[index];
-    }
-
-    /**
-     * @typedef {Object} DetailedPosition
-     * @property {HTMLElement} node
-     * @property {Number} position
-     */
-
-    /**
      * @name Caret
      */
     function Caret(element) {
         this._element = element;
-        this._isContentEditableElement = element.contentEditable === 'true';
+
+        if (this._element.contentEditable === "true") {
+            this._isContentEditableElement = true;
+        }
     }
 
     /**
@@ -44,11 +25,11 @@
      *
      * @returns {Number}
      */
-    Caret.prototype.getPosition = function () {
+    Caret.prototype.getPosition = function() {
         if (this._isContentEditableElement) {
-            this._element.focus();
-            var range = window.getSelection().getRangeAt(0);
-            var rangeClone = range.cloneRange();
+            this._focus();
+            var range = window.getSelection().getRangeAt(0),
+                rangeClone = range.cloneRange();
             rangeClone.selectNodeContents(this._element);
             rangeClone.setEnd(range.endContainer, range.endOffset);
             return rangeClone.toString().length;
@@ -62,17 +43,17 @@
      *
      * @param {Number} position
      */
-    Caret.prototype.setPosition = function (position) {
+    Caret.prototype.setPosition = function(position) {
         if (position < 0) {
             position = this._element.textContent.length + position;
         }
 
-        this._element.focus();
+        this._focus();
 
         if (this._isContentEditableElement) {
-            var detailedPosition = findDetailedPosition(this._element, position);
-            if (detailedPosition) {
-                window.getSelection().collapse(detailedPosition.node, detailedPosition.position);
+            var relativePosition = getRelativePosition(this._element, position);
+            if (relativePosition) {
+                window.getSelection().collapse(relativePosition.node, relativePosition.position);
             }
         } else {
             this._element.setSelectionRange(position, position);
@@ -80,20 +61,38 @@
     };
 
     /**
-     * Find child node of maximal nesting and caret position therein by position in parent node.
+     * Set focus to element.
+     */
+    Caret.prototype._focus = function() {
+        if (document.activeElement !== this._element) {
+            this._element.focus();
+        }
+    };
+
+    /**
+     * @typedef {Object} RelativePosition
+     * @property {HTMLElement} node
+     * @property {Number} position
+     */
+
+    /**
+     * Get relative position (relative to node of maximal nesting) of caret
+     * by absolute position in any parent node.
+     *
      * @param {HTMLElement} node
      * @param {Number} position
-     * @returns {DetailedPosition} detailedPosition
+     * @returns {RelativePosition} relativePosition
      */
-    function findDetailedPosition(node, position) {
-        var currentChildOffset = 0;
-        var childs = node.childNodes;
-        for (var i = 0; i < childs.length; i++) {
-            var child = childs[i];
+    function getRelativePosition(node, position) {
+        var currentChildOffset = 0,
+            childs = node.childNodes,
+            i, child, relativePosition;
+        for (i = 0; i < childs.length; i++) {
+            child = childs[i];
             if (child.childNodes.length > 0) {
-                var detailedPosition = findDetailedPosition(child, position - currentChildOffset);
-                if (detailedPosition) {
-                    return detailedPosition;
+                relativePosition = getRelativePosition(child, position - currentChildOffset);
+                if (relativePosition) {
+                    return relativePosition;
                 }
             } else if (position <= currentChildOffset + child.textContent.length) {
                 return {
